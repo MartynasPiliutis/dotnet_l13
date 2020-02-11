3c508df5272ba5b7e9df3aaa3634635b411be68e
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-02-11 20:12:22 +0200

    Created ReportEvents.cs
    Created ReportTotalHours.cs
    Started ReportGenerator.cs

---
+++ PassControlLibrary/ReportTotalHours.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassControlLibrary
+{
+    class ReportTotalHours
+    {
+        public string NameSurname { get; private set; }
+        public string TotalHours { get; private set; }
+
+        public ReportTotalHours(string nameSurname, int totalHours)
+        {
+            NameSurname = nameSurname;
+            TotalHours = Convert.ToString(totalHours);
+        }
+    }
+}
--- PassControlManagement/Program.cs
+++ PassControlManagement/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PassControlLibrary;
 
 namespace PassControlManagement
 {
@@ -10,6 +11,12 @@ namespace PassControlManagement
     {
         static void Main(string[] args)
         {
+            /*long id;
+            id = Convert.ToInt64($"{DateTime.Now.Year}{DateTime.Now.Month}{DateTime.Now.Day}{DateTime.Now.Hour}{DateTime.Now.Minute}{DateTime.Now.Second}{DateTime.Now.Millisecond}");
+            Console.WriteLine($"{id}");
+            Console.WriteLine(Guid.NewGuid());
+            Console.ReadLine();*/
+            
         }
     }
 }
